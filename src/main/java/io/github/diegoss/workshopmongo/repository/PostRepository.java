db5db811d4033783fbf6481c3add214708ceb496
Diego <diego_sousa.s@outlook.com>
2022-05-23 21:26:48 -0300

    Consulta usando @Query

--- src/main/java/io/github/diegoss/workshopmongo/repository/PostRepository.java
+++ src/main/java/io/github/diegoss/workshopmongo/repository/PostRepository.java
@@ -2,6 +2,7 @@ package io.github.diegoss.workshopmongo.repository;
 
 import io.github.diegoss.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,9 @@ import java.util.List;
 public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByTitleContainingIgnoreCase(String pattern);
+
+    //alternativo ao primeiro
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String pattern);
+
 }
